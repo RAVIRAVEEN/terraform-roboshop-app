8f2f7a1c0c6088d8563ecfde91a2de9f09a6776f
localstar1996 <localstar1996@gmail.com>
2024-07-16 19:06:08 +0530

    update

---
+++ variables.tf
@@ -0,0 +1,52 @@
+variable "common_tags" {
+    # type = map
+    # default = {
+    #     project = "roboshop"
+    #     envinorment = "dev"
+    #     terraform = "true"
+    # }
+}
+
+variable "tags" {
+  #default = { }
+  
+}
+  variable "zone_name" {
+    #default = "devopsrank.online"
+    
+  }
+
+variable "project_name" { 
+  #default = "roboshop"
+}
+
+variable "environment" {
+  #default = "dev"
+}
+
+variable "component_sg_id" {
+  
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "private_subnet_ids" {
+  
+}
+
+variable "iam_instance_profile" {
+  
+}
+
+variable "app_alb_listener_arn" {
+  
+}
+variable "rule_priority" {
+   
+ }
+
+variable "app_version" {
+  
+}
